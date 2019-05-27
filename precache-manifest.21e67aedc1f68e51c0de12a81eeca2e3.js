422e33ea0a7449a632e25eb0313e160f7644e975
jerryc96 <jerryycheng@gmail.com>
2019-05-27 16:44:21 -0400

    Updates

---
+++ precache-manifest.21e67aedc1f68e51c0de12a81eeca2e3.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69bdf05d64ebbc6e1d01c11c5bf437ca",
+    "url": "/sprite-finder/index.html"
+  },
+  {
+    "revision": "34085d4027744b60edb1",
+    "url": "/sprite-finder/static/js/main.d94ff1dd.chunk.js"
+  },
+  {
+    "revision": "ac926724b425ca8bee79",
+    "url": "/sprite-finder/static/js/runtime~main.3fb8c9a1.js"
+  }
+]);
